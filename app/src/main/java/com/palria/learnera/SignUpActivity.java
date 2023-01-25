0460f011d1318d0e093cce97ec1b3a62b8edb0a6
Palria <palria.learnera@gmail.com>
2023-01-26 02:41:07 +1200

    for new commits

--- app/src/main/java/com/palria/learnera/SignUpActivity.java
+++ app/src/main/java/com/palria/learnera/SignUpActivity.java
@@ -35,7 +35,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        initUI();
 //
         signUpActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
