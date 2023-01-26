e47ae11b90544b33023349328e09204e77668ffa
gconnection <palria.learnera@gmail.com>
2023-01-26 16:04:38 +0545

    sign in ui added

--- app/src/main/java/com/palria/learnera/MainActivity.java
+++ app/src/main/java/com/palria/learnera/MainActivity.java
@@ -2,7 +2,13 @@ package com.palria.learnera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Objects;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
         initUI();
         initializeApp();
     }
@@ -21,6 +29,35 @@ public class MainActivity extends AppCompatActivity {
      * to avoid null pointer exception.
      * */
     private void initUI(){
+
+        //get login and register test button by idj
+
+        Button login_test = (Button) findViewById(R.id.login_test_id);
+        Button register_test = (Button) findViewById(R.id.register_test_id);
+
+        //set test login button click listener
+
+        login_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), SignInActivity.class);
+                startActivity(i);
+
+
+            }
+        });
+
+        //set test register button click listener.
+        register_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), SignUpActivity.class);
+                startActivity(i);
+
+
+            }
+        });
+
 
     }
     /**<p>Initializes global variables which will be shared across every activities</p>
--- app/src/main/java/com/palria/learnera/SignInActivity.java
+++ app/src/main/java/com/palria/learnera/SignInActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SignInActivity extends AppCompatActivity {
 private String email;
@@ -14,9 +17,15 @@ private String password;
 private EditText emailEditText;
 private EditText passwordEditText;
 private Button signInActionButton;
+//register and forget password link
+private TextView register_link_view;
+private TextView forget_password_link;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_sign_in);
         //initializes this activity's views
         initUI();
@@ -48,10 +57,39 @@ private Button signInActionButton;
                 });
             }
         });
+
+        //register link button click listener
+        register_link_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //regiser|sign up activity starts from here .
+                Intent i = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+        forget_password_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //forget password activity intent starts from here .
+            }
+        });
     }
 
 
     private void initUI(){
+
+
+        emailEditText = (EditText) findViewById(R.id.emailInput);
+        passwordEditText = (EditText) findViewById(R.id.passwordInput);
+
+        signInActionButton = (Button) findViewById(R.id.loginButton);
+
+        register_link_view = (TextView) findViewById(R.id.register_link);
+
+        forget_password_link = (TextView) findViewById(R.id.forget_password_link);
+
+
 
     }
 
